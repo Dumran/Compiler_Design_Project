2ce54478bfe9e83fe26b7d1578d33e56d9dc1210
Gülçin BODUR <gulc@Gulc-MacBook-Pro.local>
2024-12-15 12:02:59 +0300

    main.c has been seperated

--- interpreter.c
+++ interpreter.c
@@ -676,22 +676,3 @@ static int F() {
     }
     return 0;
 }
-
-int main() {
-    const char* programText =
-        "n = 0;\n"
-        "{ n - 2*5 ?\n"
-        "  < n;\n"
-        "  n = n + 1;\n"
-        "}\n"
-        ".\n";
-
-    input = programText;
-    position = 0;
-    memset(variables, 0, sizeof(variables));
-
-    getNextToken();
-    P();
-
-    return 0;
-}
